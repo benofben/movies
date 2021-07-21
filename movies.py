36ba02cc7f27d696ba629c4b02e73ed136c21c4d
Ben Lackey <ben.lackey@outlook.com>
2021-07-21 11:42:59 -0700

    all pep'd

--- movies.py
+++ movies.py
@@ -2,8 +2,8 @@ import sys
 from rockset import Client, ParamDict
 from tabulate import tabulate
 
-if len(sys.argv)==2:
-    year=sys.argv[1]
+if len(sys.argv) == 2:
+    year = sys.argv[1]
     print('Querying Rockset for year ' + year + '...')
 else:
     print('usage: python3 movies.py [year]')
@@ -13,17 +13,20 @@ params = ParamDict()
 params['year'] = year
 rs = Client()
 
-queries=[
-    {'message':'The top 10 most popular movies for ', 'query':'mostpopular', 'version':'4a9eeaec908830b0'},
-    {'message':'The top 10 highest grossing movies for ', 'query':'highestgrossing', 'version':'9ed282f82b8d18c3'},
-    {'message':'The top 10 genres for ', 'query':'topgenres', 'version':'ff13cce933698574'},
-    {'message':'The top production companies for the most popular genre of ', 'query':'topproductioncompanies', 'version':'67a8f1531a780120'}]
+queries = [
+    {'message': 'The top 10 most popular movies for ', 'query': 'mostpopular', 'version': '4a9eeaec908830b0'},
+    {'message': 'The top 10 highest grossing movies for ', 'query': 'highestgrossing', 'version': '9ed282f82b8d18c3'},
+    {'message': 'The top 10 genres for ', 'query': 'topgenres', 'version': 'ff13cce933698574'},
+    {'message': 'The top production companies for the most popular genre of ', 'query': 'topproductioncompanies', 'version': '67a8f1531a780120'}]
+
 
 def query(q):
     print('\n' + q['message'] + year + ' are...')
-    qlambda = rs.QueryLambda.retrieve(q['query'],version=q['version'], workspace='commons')
+    qlambda = rs.QueryLambda.retrieve(
+        q['query'], version=q['version'], workspace='commons')
     results = qlambda.execute(parameters=params)
     print(tabulate(results['results'], headers='keys'))
+
 
 for q in queries:
     query(q)
